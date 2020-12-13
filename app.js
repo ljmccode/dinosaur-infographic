ce69acac7ac9a01530494df03900611cd805bcb3
Laura <lmcsay@gmail.com>
2020-12-13 15:04:55 -0600

    Create dinos array from JSON file

--- app.js
+++ app.js
@@ -17,6 +17,30 @@ const getDinoData = async () => {
 };
 
 // Create Dino Objects
+const dinos = [];
+
+window.onload = async function () {
+  const dinoArr = await getDinoData();
+  dinoArr.forEach((dino) => {
+    dinos.push(
+      new Dino(
+        dino.species,
+        dino.weight,
+        dino.height,
+        dino.diet,
+        dino.where,
+        dino.when,
+        dino.fact
+      )
+    );
+  });
+};
+
+function readDinos() {
+  console.log(dinos);
+}
+
+readDinos();
 
 // Create Human Object
 
