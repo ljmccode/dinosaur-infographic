23a4eaebd115fa8fd01e3c3f86139e332f90d427
Laura <lmcsay@gmail.com>
2020-12-13 14:48:16 -0600

    Add Dino constructor

--- app.js
+++ app.js
@@ -1,32 +1,40 @@
+//  Dino Constructor
+function Dino(species, weight, height, diet, where, when, fact) {
+  this.species = species;
+  this.weight = weight;
+  this.height = height;
+  this.diet = diet;
+  this.where = where;
+  this.when = when;
+  this.fact = fact;
+}
 
-    // Create Dino Constructor
+// Grab Dinos from JSON file
+const getDinoData = async () => {
+  const fetchedData = await fetch("./dino.json");
+  const data = await fetchedData.json();
+  return data.Dinos;
+};
 
+// Create Dino Objects
 
-    // Create Dino Objects
+// Create Human Object
 
+// Use IIFE to get human data from form
 
-    // Create Human Object
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    // Use IIFE to get human data from form
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
+// Generate Tiles for each Dino in Array
 
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+// Add tiles to DOM
 
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
-
-    // Remove form from screen
-
+// Remove form from screen
 
 // On button click, prepare and display infographic
